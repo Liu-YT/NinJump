e6a382756b05bdbf8e2a8f7e15fcdbc6f1caf3b5
LiuYT <31623337+Liu-YT@users.noreply.github.com>
2018-07-20 13:02:59 +0800

    页面初步完成“

---
+++ Classes/GameScene.cpp
@@ -0,0 +1,32 @@
+#include "GameScene.h"
+
+USING_NS_CC;
+
+Scene* GameScene::createScene()
+{
+	return GameScene::create();
+}
+
+// on "init" you need to initialize your instance
+bool GameScene::init()
+{
+	//////////////////////////////
+	// 1. super init first
+	if (!Scene::init())
+	{
+		return false;
+	}
+
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	auto bg = Sprite::create("images/background.jpg");
+	bg->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+	bg->setScale(visibleSize.width / bg->getContentSize().width,
+		visibleSize.height / bg->getContentSize().height);
+	this->addChild(bg, 0);
+
+
+
+	return true;
+}
